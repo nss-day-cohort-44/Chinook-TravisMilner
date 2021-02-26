054ca559a4d3049decc920de3491a82df4f4c716
Travis Milner <travismilner1@gmail.com>
2021-02-26 10:23:30 -0600

    added line 37 query

---
+++ invoice_37_line_item_count.sql
@@ -0,0 +1,11 @@
+select * from InvoiceLine
+
+select count(InvoiceLineId) from InvoiceLine 
+Where InvoiceId = 37
+
+
+
+
+
+" Looking at the InvoiceLine table, provide a query that COUNTs 
+the number of line items for Invoice ID 37."
